b53756676787d708199e8f26cd0d5e16ee94bafc
elmatematic0 <wkdalebout+github@gmail.com>
2013-10-23 14:19:35 -0400

    adding length to groceries method.

--- exercise6.rb
+++ exercise6.rb
@@ -2,7 +2,7 @@ def groceries
   grocery_list = ["carrots", "toilet paper", "apples", "fish"]
   grocery_list << "rice"
   grocery_list.each {|a| puts "* #{a}"}  
-
+  puts grocery_list.length
 end
 
-groceries 
+groceries
