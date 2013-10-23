332f0a485894d6467af70adcb8dace7b09d2601d
elmatematic0 <wkdalebout+github@gmail.com>
2013-10-23 14:29:29 -0400

    checking to see if bananas need to be picked up

--- exercise6.rb
+++ exercise6.rb
@@ -3,6 +3,11 @@ def groceries
   grocery_list << "rice"
   grocery_list.each {|a| puts "* #{a}"}  
   puts grocery_list.length
+  if grocery_list.include?("bananas") != true
+    puts "You do not need to pick up bananas today"
+  else
+    puts "You need to pick up bananas"
+  end
 end
 
 groceries
