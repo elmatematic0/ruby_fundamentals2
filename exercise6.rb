9eaf0f1f975271f6411b19244b3107f3e90f4d28
elmatematic0 <wkdalebout+github@gmail.com>
2013-10-23 15:26:24 -0400

    refactored if statement

--- exercise6.rb
+++ exercise6.rb
@@ -3,15 +3,16 @@ def groceries
   grocery_list << "rice"
   grocery_list.each {|a| puts "* #{a}"}  
   puts grocery_list.length
-  if grocery_list.include?("bananas") != true
-    puts "You do not need to pick up bananas today"
-  else
+  if grocery_list.include?("bananas")
     puts "You need to pick up bananas"
+  else
+    puts "You do not need to pick up bananas today"
   end
+
   puts grocery_list[1]
   grocery_list.sort.each {|a| puts "* #{a}"}
   grocery_list.delete("fish")
   grocery_list.sort.each {|a| puts "* #{a}"}
-end
 
+end
 groceries
