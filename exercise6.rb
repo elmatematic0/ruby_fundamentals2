44fc7a44a7f6caf3203241ba51035616a692a301
elmatematic0 <wkdalebout+github@gmail.com>
2013-10-23 14:14:34 -0400

    adding rice to the array and creating method.

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,8 @@
-grocery_list = ["carrots", "toilet paper", "apples", "fish"]
-grocery_list.each {|a| puts "* #{a}"}
+def groceries
+  grocery_list = ["carrots", "toilet paper", "apples", "fish"]
+  grocery_list << "rice"
+  grocery_list.each {|a| puts "* #{a}"}  
+
+end
+
+groceries 
